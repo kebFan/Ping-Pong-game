8d843c99749b2c065cfcbb2b5d2593b512303089
kebFan <keburefantaye@gmail.com>
2016-10-05 15:43:38 -0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPong
+{
+    public partial class Form1 : Form
+    {
+        public int speed_left = 4;  // speed of the ball
+        public int speed_top = 4;
+        private int points = 0;           // score point
+
+        public int Point
+        {
+            get
+            {
+                return points;
+            }
+
+            set
+            {
+                points = value;
+            }
+        }
+
+        public int Point1
+        {
+            get
+            {
+                return points;
+            }
+
+            set
+            {
+                points = value;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Enabled = true;
+            Cursor.Hide();   // hide the cursor
+
+            this.FormBorderStyle = FormBorderStyle.None; // removes any border
+            this.TopMost = true;   // brings the form to the front
+            this.Bounds = Screen.PrimaryScreen.Bounds;   // make it fullscreen
+
+            racket.Top = playground.Bottom - (playground.Bottom / 10);
+
+            gameover_lbl.Left = (playground.Width / 2) - (gameover_lbl.Width/2);  //position to center
+            gameover_lbl.Top = (playground.Height / 2) - (gameover_lbl.Height / 2);
+            gameover_lbl.Visible = false; //hide
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            racket.Left = Cursor.Position.X - (racket.Width / 2);
+                //moves the ball
+            ball.Left += speed_left;
+            ball.Top += speed_top;
+
+            if(ball.Bottom >= racket.Top && ball.Bottom <= racket.Bottom && ball.Left >= racket.Left && ball.Right <= racket.Right) // Racket collision
+            {
+                speed_top += 2;
+                speed_left += 2;
+                speed_top = -speed_top;  // change direction 
+                Point += 1;
+
+                points_lbl.Text = points.ToString();
+
+                Random r = new Random();
+                
+                playground.BackColor = Color.FromArgb(r.Next(150, 255), r.Next(150, 255), r.Next(150, 255));
+                ball.BackColor = Color.FromArgb(r.Next(150, 255), r.Next(150, 255), r.Next(150, 255));
+            }
+
+            if(ball.Left <= playground.Left)
+            {
+                speed_left = -speed_left;
+            }
+            if (ball.Right >= playground.Right)
+            {
+                speed_left = -speed_left;
+            }
+            if (ball.Top <= playground.Top)
+            {
+                speed_top= -speed_top;
+            }
+            if(ball.Bottom >= playground.Bottom)
+            {
+                timer1.Enabled = false;  //ball is out stop the game
+                gameover_lbl.Visible = true;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Escape) { this.Close(); } // press escape to quit
+            if(e.KeyCode == Keys.F1)
+            {
+                ball.Top = 50;
+                ball.Left = 50;
+                speed_left = 4;
+                speed_top = 4;
+                points = 0;
+                points_lbl.Text = "0";
+                timer1.Enabled = true;
+                gameover_lbl.Visible = false;
+            }
+        }
+
+    }
+}
